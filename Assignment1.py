56fd6788ab86b43e0bd97ca23b7840bddbe4485e
gaaraujo <gaaraujor@gmail.com>
2025-01-12 09:28:52 -0800

    Change the code to accept the number of lines as a command line argument (e.g., running python3 pascal.py 6 should print 6 lines).

--- Assignment1.py
+++ Assignment1.py
@@ -3,6 +3,7 @@
 # Modified by: Gustavo A. Araújo R.
 
 from math import comb
+import argparse
 
 def pascal_triangle(n):
     for i in range(n):
@@ -12,4 +13,14 @@ def pascal_triangle(n):
             print(comb(i,j), end=" ")
         print("\n")
 
-pascal_triangle(15)
+if __name__ == '__main__':
+    # Parse arguments
+    parser = argparse.ArgumentParser(description="Print `n_rows` lines from Pascal's triangle and optionally time how long it takes to generate it.")
+    parser.add_argument('n_rows', metavar='n_rows', type=int, nargs="?", default=15,
+                    help="number of lines to print from Pascal's triangle")
+    args = parser.parse_args()
+    parser.print_help()    
+    
+    # Print lines
+    print(f"\nOutput: These are the first {args.n_rows} lines of Pascal's triangle ")
+    pascal_triangle(args.n_rows)
